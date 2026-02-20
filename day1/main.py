de39c9d4d469e07f94ba57c00060f6d530c53955
baliram072x <baliramrathod0008@gmail.com>
2026-02-20 10:50:57 +0530

    day2

--- day1/main.py
+++ day1/main.py
@@ -1,1 +1,15 @@
 print("hello , Baliram")
+print(5)
+print(5+7)
+
+print("hey i am a good boy \nand this viewer is also a good boy ")
+
+# \n ==> use to new line in txt this known as escape sequence 
+# comments
+# this is a single line comment
+
+print("hey", 6,7 , sep="~" ,  end="009\n")
+# sep ==> use to separate the value with any symbol
+# end ==> use to end the line with any symbol
+print("baliram")
+
---
+++ day2/main.py
@@ -0,0 +1,24 @@
+a = 12334445
+print (a)
+
+b = "baliram"
+print (b)
+ 
+c  =  True
+d = None 
+
+print ("type of a is " ,  type (a))
+print ("type of b is " ,  type (b))
+print ("type of b is " ,  type (c))
+print ("type of b is " ,  type (d))
+
+list1 =  [1,2,3,[-1,5] ,["baliram", "kumar"]] 
+print (list1)
+
+tuple = (("parrot , sparrow ") , ("lion ,  tiger "))
+print (tuple)
+
+dict1 = {"name" : "baliram" , "age" : 19 , "city" : "pune" ,  "CanVote" : True }
+print (dict1)
+
+
