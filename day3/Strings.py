3d5ecd69c0b133dd1301660318abe5b3479d0cba
baliram072x <baliramrathod0008@gmail.com>
2026-03-16 04:34:48 +0530

    4.34

---
+++ day3/Strings.py
@@ -0,0 +1,9 @@
+name = "Baliram"
+friend = "Trupti"
+
+apple = '''multiline Strings are created using three single or double quotes.
+This is the second line of the string. 
+This is the third line of the string'''
+
+print(apple)
+
