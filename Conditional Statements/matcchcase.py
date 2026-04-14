c753047c418da9e3a837d063ea5a9dfa4f871546
baliram072x <baliramrathod0008@gmail.com>
2026-04-14 20:39:39 +0530

    match statement

---
+++ Conditional Statements/matcchcase.py
@@ -0,0 +1,15 @@
+x = 4
+
+match x:
+    # case _ if x !=90:
+    #     print("x is 1")
+    
+    case 2:
+        print("x is 2 ")
+
+    case 3:
+        ("case is 3 ")
+
+    case 4:
+        print("x is 4")
+
