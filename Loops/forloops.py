9654a6f68103e027d00f9a8b3ae26490b9b14a87
baliram072x <baliramrathod0008@gmail.com>
2026-04-16 04:32:23 +0530

    16/04

---
+++ Loops/forloops.py
@@ -0,0 +1,14 @@
+# name = 'Baliram'
+
+# for i in name:
+#     print(i)
+   
+
+for k in range(5):
+    print(k+1)
+
+for k in range(1, 9):
+    print(k)
+
+for k in range(1, 10, 3):  #search about this
+    print(k)
---
+++ Loops/whileloopss.py
@@ -0,0 +1,15 @@
+
+
+# i = 0 
+# while i<3:
+#     print(i)
+#     i = i+1
+
+
+count = 5
+
+while count >0:
+    print(count)
+    count = count -1
+else : 
+    print("ho gya bhai tera loop khatam")
