32cf90b06a6d8e44b0e5973656483b1879f2d2cd
baliram072x <baliramrathod0008@gmail.com>
2026-04-16 04:44:51 +0530

    16/04

---
+++ Loops/BreakOrContinueStatement.py
@@ -0,0 +1,20 @@
+# break statement 
+# ut break statement is used to exit the loop when a certain condition is met.
+
+# for i in range(12): 
+#     if(i == 10):
+#         break
+#     print("5 x ", i, " = ", 5*(i))
+
+# print("loop ko chodke nikal gya ")
+
+
+for i in range(12): 
+    if(i == 10):
+        print("10 aagya bhai, continue kar raha hu ")
+        continue
+    print("5 x ", i, " = ", 5*(i))
+
+
+
+
--- Loops/whileloopss.py
+++ Loops/whileloopss.py
@@ -6,10 +6,18 @@
 #     i = i+1
 
 
-count = 5
+# count = 5
 
-while count >0:
-    print(count)
-    count = count -1
-else : 
-    print("ho gya bhai tera loop khatam")
+# while count >0:
+#     print(count)
+#     count = count -1
+# else : 
+#     print("ho gya bhai tera loop khatam")
+
+i = 0
+while True:
+    print(i)
+    i = i+1
+    if i%100 == 0:
+        break
+print("loops breaks ")
